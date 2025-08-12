cc7ba7ad8ebed0a9334e6f2fc6d6ef70c1d8a7a0
Carlos_Rodriguez <carlos_3141240128@utd.edu.mx>
2025-08-12 17:32:11 -0600

    Delete PF directory

--- PF/PF/Casino/globales.py
+++
@@ -1,4 +0,0 @@
-dinero_cliente = 0
-telefono_cliente=0
-id_juego=6
-id_cliente=1
--- PF/PF/Casino/juegos/juegos.py
+++
@@ -1,6 +0,0 @@
-def borrarPantalla():
-    import os
-    os.system("cls")
-
-def espereTecla():
-    input(f"\n\t... Oprima cualquier tecla pra continuar ...")
